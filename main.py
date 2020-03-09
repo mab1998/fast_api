4fc65326f5ae2786266900927353c239030e06b2
mabrouk <mabrouk.bennaoui@gmail.com>
2020-03-09 22:07:04 +0100

    post /templat

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+class Item(BaseModel):
+    name: str
+    imageUrl: str = None
+    jsondata: str
+    status: str = None
 
+# @app.post("/templat")
+# async def root(name,imageUrl,jsondata,status):
+#     return {"message": "Hello World"}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/templat/")
+async def create_item(item: Item):
+    return item
